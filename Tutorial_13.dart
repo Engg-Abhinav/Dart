bc2ee46c991b0d1a09d5f5203109586bc1cb5669
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-02-04 22:57:25 +0530

    Map

--- Tutorial_13.dart
+++ Tutorial_13.dart
@@ -1,1 +1,28 @@
-!
+// Collection
+
+// Map - It's a collection of key value pair of items in some programming language,
+// like in python it's called dictionary.
+
+void main(){
+  var cricket = {
+    'first': 'MS Dhoni',
+    'second': 'Virat Kohli',
+    'third': 'Rohit Sharma'
+  };
+
+  // var cricket = {
+  //   1: 'MS Dhoni',
+  //   2: 'Virat Kohli',
+  //   3: 'Rohit Sharma'
+  // };
+
+  print(cricket['first']);
+  // print(cricket[1]);
+
+  var fruits = Map();
+  fruits['first'] = 'Mango';
+  print(fruits['first']);
+
+  var vegetable = {'first': 'Potato', 'second': 'Cabbage'};
+  print(vegetable['first']);
+}
