11c48737a9c287717de12a68d6acf6c123312df0
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-02-04 23:26:20 +0530

    Function Part-2

--- Tutorial_15.dart
+++ Tutorial_15.dart
@@ -1,1 +1,26 @@
-!
+// Function
+// Function with no name is known as anynomous function
+
+// main(){
+//   var list = ['apples', 'grapes', 'oranges'];
+
+//   list.forEach(printF);
+// }
+
+// void printF(item){
+//   print(item);
+// }
+
+main(){
+  var list = ['apples', 'grapes', 'oranges'];
+
+  list.forEach((item){
+    print(item);
+  });
+
+  // The Function 
+  // '(item){
+  //   print(item);
+  // }'
+  // is an anynomous Function
+}
