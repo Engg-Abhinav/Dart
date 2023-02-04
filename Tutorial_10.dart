f18977417b98b0095fc9ed604eef9b6f2937e59c
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-02-04 13:07:32 +0530

    Break and Continue

--- Tutorial_10.dart
+++ Tutorial_10.dart
@@ -1,1 +1,13 @@
-!
+// Break and Continue
+
+void main(){
+  // for(var i=0;i<=10;i++){
+  //   if (i >5) break;
+  //   print(i);
+  // }
+
+  for (var i = 0; i < 10; i++){
+    if (i % 2 != 0) continue;
+    print("Even: $i");
+  }
+}
