6b728fe26950ede6bd3a37cc77729c24842fec20
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-02-04 23:17:31 +0530

    Function Part-1

--- Tutorial_14.dart
+++ Tutorial_14.dart
@@ -1,1 +1,20 @@
-!
+// Function - In dart, each function is an object of class function where F is capital
+
+void main(){
+  showOutput(square(2));
+  showOutput(square(2.5));
+  showOutput(cube(2));
+  print(square.runtimeType);
+  print(cube.runtimeType);
+}
+
+dynamic square(var num){
+  return num*num;
+}
+
+// Arrow Function: =>
+dynamic cube(var num) => num*num*num;
+
+void showOutput(var msg){
+  print(msg);
+}
