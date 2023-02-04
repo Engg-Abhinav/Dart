ca4957bd2f5656db88be752aeade3a323ca12df9
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-02-04 11:14:27 +0530

    Operators
    Ternary and Type Test operators

--- Tutorial_7.dart
+++ Tutorial_7.dart
@@ -1,1 +1,15 @@
-!
+// Ternary Operator
+
+void main(){
+  int x = 100;
+  // int x = 101;
+  var result = x % 2 == 0 ? 'Even':'Odd';
+  print(result);
+
+  // Type Test Operator
+  var y = 100;
+  // var y = 100.00;
+  if (y is int){
+    print('integer');
+  }
+}
