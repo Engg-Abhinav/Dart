1a2e2da1c9bd9d2e7267c9b48b6a1e1612c94f21
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-02-04 12:57:33 +0530

    Loops

--- Tutorial_9.dart
+++ Tutorial_9.dart
@@ -1,1 +1,32 @@
-!
+void main(){
+  // For Loop
+  // for(var i=1; i<=10; i++){
+  //   print(i);
+  // }
+
+  // For...in Loop
+  // var numbers = [1, 2, 3];
+  // for (var n in numbers){
+  //   print(n);
+  // }
+  // for (var i=0; i<numbers.length; ++i){
+  //   print(numbers[i]);
+  // }
+
+  // For Each Loop
+  // var numbers = [1, 2, 3];
+  // numbers.forEach((n)=>print(n));  // (n)=>print(n) is Arrow Function
+
+  // While Loop
+  int num = 5;
+  while (num>0){
+    print(num);
+    num-=1;
+  }
+
+  // do...While Loop
+  do{
+    print(num);
+    num-=1;
+  }while(num>0);
+}
