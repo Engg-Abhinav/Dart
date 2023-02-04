94ed7f98d36a44b94b257ae894f3d5394351cd6e
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-02-04 22:43:08 +0530

    Set

--- Tutorial_12.dart
+++ Tutorial_12.dart
@@ -1,1 +1,20 @@
-!
+// Collection
+// Set - Collection of unique item
+
+import 'dart:collection';
+
+void main(){
+//   var halogens = {'Fluorine', 'Chlorine', 'Fluorine'};
+  
+//   for(var x in halogens){
+//     print(x);
+//   }
+
+  // var halogens = LinkedHashSet<String>();
+  var halogens = HashSet<String>();
+  print(halogens.runtimeType);
+
+  Set <String> names = {};
+  print(names.runtimeType);
+
+}
