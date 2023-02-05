8f399157af999d9fe54c918c2fd8e592ade5e1d3
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-02-05 18:37:42 +0530

    Class Part-3

--- Tutorial_19.dart
+++ Tutorial_19.dart
@@ -1,1 +1,33 @@
-!
+// Class
+// Class inheritance
+
+class Vehicle{
+  String model;
+  int year;
+
+  Vehicle(this.model, this.year){
+    print(this.model);
+    print(this.year);
+  }
+
+  void showOutput(){
+    print(model);
+    print(year);
+  }
+}
+
+class Car extends Vehicle{
+  double price;
+
+  Car(String model, int year, this.price) : super(model, year);
+
+  void showOutput(){
+    super.showOutput();
+    print(this.price);
+  }
+}
+
+main(){
+  var car1 = Car('Verna', 2014, 150000);
+  car1.showOutput();
+}
