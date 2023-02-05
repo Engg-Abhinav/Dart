6bdec064febe8fda4c0946a2d96531d90edc05ca
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-02-05 22:53:15 +0530

    Exception Handling

--- Tutorial_21.dart
+++ Tutorial_21.dart
@@ -1,1 +1,34 @@
-!
+// Exception Handling - We can use throw try catch finally keyword
+
+int mustGreaterThanZero(int n){
+  if(n <= 0){
+    throw Exception("Value must be greater than zero");
+  }
+  return n;
+}
+
+void letGetItVerified(int n){
+  var valueVerification;
+
+  try{
+    valueVerification = mustGreaterThanZero(n);
+  }
+  // If our program has some specific type of error then we can use 'on' keyword
+  catch(e){
+    print(e);
+  }
+  // Optional
+  finally{
+    if(valueVerification == null){
+      print('Value is not accepted');
+    }
+    else{
+      print('Value verified: $valueVerification');
+    }
+  }
+}
+
+void main(){
+  letGetItVerified(10);
+  // letGetItVerified(0);
+}
